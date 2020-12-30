5259b93f92eb95590ab03721d8372271066fef73
C. L <amautacelt@gmail.com>
2020-12-30 07:18:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -41,7 +41,7 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   
   planeteer_calls.find do |element|
-    element == 
+    planeteer_calls
   end
   
 end
