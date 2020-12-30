ccb47eb047538ae0664f1156d029f817114b8d13
C. L <amautacelt@gmail.com>
2020-12-30 06:11:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,8 @@ def summon_captain_planet(planeteer_calls)
   # Return a new array of the results
   
   planeteer_calls.map do |element|
-    print "#{element.capitalize}!"
+    new_array = []
+    puts new_array << "#{element.capitalize}!"
   end
   planeteer_calls
 end
